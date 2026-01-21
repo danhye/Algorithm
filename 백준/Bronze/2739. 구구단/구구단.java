d7daf91046a9e7762ece65ebcb7aaad86663d5a2
Lee Danhye <dan0628@naver.com>
2026-01-21 22:57:43 +0900

    [Bronze V] Title: 구구단, Time: 132 ms, Memory: 16208 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -2,14 +2,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main{
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int num = Integer.parseInt(br.readLine());
-		
-		for(int i=1;i<10;i++) {
-			System.out.println(num + " * " + i + " = " + num*i);
-		}
-	}
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N =  Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= 9; i++){
+            System.out.println( N + " * " + i + " = " + N*i);
+        }
+    }
 }
