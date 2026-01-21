c28c4b0fd4c1c1de3e12a40ae82de3419288be00
Lee Danhye <dan0628@naver.com>
2026-01-21 22:45:43 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 180 ms, Memory: 14960 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,20 +1,18 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int input = Integer.parseInt(br.readLine());	
-		
-		// 1행 -> 한개
-		// 2행 -> 두개
-		// 3행 -> 3개     n 행까지 반복 n개 출력
-		for(int i=1;i<=input;i++) {
-			for(int j=0;j<i;j++) {
-				System.out.print("*");
-			}
-			System.out.println("");
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
