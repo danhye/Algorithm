1005280e8c6c06b7ae888ddfc6a9ad72a12485c8
Lee Danhye <dan0628@naver.com>
2026-01-24 15:39:59 +0900

    [Bronze V] Title: 윤년, Time: 104 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,21 +1,18 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
-public class Main{
-    public static void main(String[] args) throws IOException{
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         int year = Integer.parseInt(br.readLine());
-        
-        if(isLeapYear(year)){
-            System.out.print("1");
-        }else{
-            System.out.print("0");
+        int ans = 0;
+
+        if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ){
+            ans = 1;
         }
-        
-    }
-    //윤년 조건 검사
-    public static boolean isLeapYear(int year){
-        return (year%4==0&&year%100!=0)||year%400==0;       
+
+        System.out.println(ans);
     }
 }
