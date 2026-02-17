67daa1114be119c646500bec0533cff481e7b98e
Lee Danhye <dan0628@naver.com>
2026-02-17 16:43:58 +0900

    [Bronze II] Title: 나머지, Time: 88 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -3,21 +3,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		boolean[] mod = new boolean[42];
-		
-		for (int i = 0; i < 10; i++) {
-			int num = Integer.parseInt(br.readLine()) % 42;
-			mod[num] = true;
-		}
-		
-		int count = 0;
-		for (int i = 0; i < mod.length; i++) {
-			if(mod[i]) count++;
-		}
-		
-		System.out.println(count);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] rest = new int[42];
+
+        for(int i = 1; i <= 10; i++){
+            int num = Integer.parseInt(br.readLine());
+            int restNum = num % 42;
+            rest[restNum]++;
+        }
+
+        int ans = 0;
+        for(int i = 0; i < rest.length ; i++){
+            if(rest[i] > 0) ans++;
+        }
+
+        System.out.println(ans);
+    }
 }
