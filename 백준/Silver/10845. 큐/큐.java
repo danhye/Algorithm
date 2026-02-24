03a160814ed1e586f472813798b82ad22012e349
Lee Danhye <dan0628@naver.com>
2026-02-24 17:33:57 +0900

    [Silver IV] Title: 큐, Time: 140 ms, Memory: 19012 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Deque<Integer> dq = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            String operator = st.nextToken();
+
+            if (operator.equals("push")) {
+                int num = Integer.parseInt(st.nextToken());
+                dq.add(num);
+            }
+            else if (operator.equals("pop")) {
+                if (!dq.isEmpty()) sb.append(dq.pollFirst()).append("\n");
+                else sb.append("-1").append("\n");
+            }
+            else if (operator.equals("size")) sb.append(dq.size()).append("\n");
+            else if (operator.equals("empty")) {
+                if (dq.isEmpty()) sb.append("1").append("\n");
+                else sb.append("0").append("\n");
+            }
+            else if (operator.equals("front")) {
+                if (dq.isEmpty()) sb.append("-1").append("\n");
+                else sb.append(dq.peekFirst()).append("\n");
+            }
+            else if (operator.equals("back")) {
+                if (dq.isEmpty()) sb.append("-1").append("\n");
+                else sb.append(dq.peekLast()).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
