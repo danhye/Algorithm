92ef13c5085446350a02e00d5cc425e94eafafc7
Lee Danhye <dan0628@naver.com>
2026-03-03 14:01:47 +0900

    [Gold V] Title: 링크와 스타트, Time: 808 ms, Memory: 20864 KB -BaekjoonHub

---
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int[][] S;
+    static boolean[] selected;
+    static int min = Integer.MAX_VALUE;
+
+    static void dfs(int idx) {
+        calcDiff();
+
+        for (int i = idx; i < N; i++) {
+            selected[i] = true;
+            dfs(i + 1);
+            selected[i] = false;
+        }
+    }
+
+    static void calcDiff() {
+
+        int cnt = 0;
+        for(int i = 0; i < N; i++) if(selected[i])cnt++;
+        if(cnt == 0 ||cnt == N) return;
+
+        int startSum = 0;
+        int linkSum = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (selected[i] && selected[j]) {
+                    startSum += S[i][j] + S[j][i];
+                } else if (!selected[i] && !selected[j]) {
+                    linkSum += S[i][j] + S[j][i];
+                }
+            }
+        }
+
+        min = Math.min(min, Math.abs(startSum - linkSum));
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        S = new int[N][N];
+        selected = new boolean[N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                S[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        selected[0] = true;
+        dfs(1);
+
+        System.out.println(min);
+    }
+}
