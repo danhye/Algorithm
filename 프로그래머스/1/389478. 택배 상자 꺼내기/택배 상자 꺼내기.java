1d1d70077791b6e0bf6c74a0f0c8836bc736698c
Lee Danhye <dan0628@naver.com>
2026-07-08 17:06:35 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.03 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,39 +1,32 @@
 class Solution {
-    static int nx, ny;
-
-    public void isTarget(int box, int num, int x, int y){
-        if(box == num) {
-            nx = x; ny = y;
-        }
-    }
-
     public int solution(int n, int w, int num) {
 
         // 2차원 배열 생성하기
         // 높이값 구하기
-        int h;
-        if(n % w != 0)  h = n / w + 1;
-        else h = n / w;
+        int h = (n + w - 1) / w;
 
-        int[][] boxs = new int[h][w];
+        int[][] boxes = new int[h][w];
 
         // 택배 쌓기
         int box = 1;
+        int nx = 0, ny = 0;
         
         out: 
         for(int i = 0; i < h; i++){
-            // 인덱스가 짝수이면 오른쪽으로 쌓기
-            if(i % 2 == 0){
+            if(i % 2 == 0){ // 인덱스가 짝수이면 오른쪽으로 쌓기
                 for(int j = 0; j < w; j++){
-                    isTarget(box, num, i, j);
-                    boxs[i][j] = box++;
+                    if(box == num){
+                        nx = i; ny = j;
+                    }
+                    boxes[i][j] = box++;
                     if(box > n) break out;
                 }
-            }
-            if(i % 2 != 0){ // 인덱스가 홀수이면 왼쪽으로 쌓기
+            }else{ // 인덱스가 홀수이면 왼쪽으로 쌓기
                 for(int j = (w-1); j >= 0; j--){
-                    isTarget(box, num, i, j);
-                    boxs[i][j] = box++;
+                    if(box == num){
+                        nx = i; ny = j;
+                    }
+                    boxes[i][j] = box++;
                     if(box > n) break out;
                 }
             }
@@ -41,7 +34,7 @@ class Solution {
 
         // 택배 빼기
         int answer = 0;
-        if(boxs[h-1][ny] != 0){
+        if(boxes[h-1][ny] != 0){
             answer = (h-1) - nx + 1;
         }else{
             answer = (h-1) - nx;
